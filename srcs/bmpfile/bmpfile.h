6188df2294dbfe560c780389852b0375006e9a29
david <dadamouyal42@gmail.com>
2020-03-24 18:21:53 +0100

    Added romain's libft

--- srcs/bmpfile/bmpfile.h
+++ srcs/bmpfile/bmpfile.h
@@ -1,18 +1,27 @@
 #ifndef BMPFILE_H
 # define BMPFILE_H
+# include "d42_sys.h"
+# define BMPFILE_HEADER "BM\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
 
 typedef enum e_bmpfile_com
 	{
 		bmpfile_ok, bmpfile_error
 	}	t_bmpfile_com;
 
-typedef struct	s_bmpfile_info
-{
-	unsigned char	*screen;
-	int				height;
-	int				width;
-}				t_bmpfile_info;
+typedef unsigned char	t_bmpfile_header[14];
+typedef unsigned char	t_bmpfile_infoheader[40];
 
-t_bmpfile_com bmpfile_save(char *filename, t_bmpfile_info *info);
-t_bmpfile_com bmpfile_load(char *filename, t_bmpfile_info *info);
+typedef struct	s_bmpfile
+{
+	t_bmpfile_header		header;
+	t_bmpfile_infoheader	infoheader
+	unsigned char			*content;
+	int						height;
+	int						width;
+	short					padding;
+}				t_bmpfile;
+
+
+t_bmpfile_com bmpfile_save(char *filename, t_bmpfile *info);
+t_bmpfile_com bmpfile_load(char *filename, t_bmpfile *info);
 #endif
